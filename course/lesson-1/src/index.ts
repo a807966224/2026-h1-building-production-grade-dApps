4ef9a3b2ecb3b7f3044ff0c65de586c1eaf556a5
Your Name <you@example.com>
2025-12-25 18:41:50 +0800

    lesson 1 homework

--- course/lesson-1/src/index.ts
+++ course/lesson-1/src/index.ts
@@ -1,9 +1,10 @@
 import { ethers } from "ethers";
 
 async function main() {
+    // faucet https://faucet.polkadot.io/ Paseo AssetHub
     const URL = "https://services.polkadothub-rpc.com/testnet";
     const provider = new ethers.JsonRpcProvider(URL);
-    const address = "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045"; // put your address here
+    const address = "0x"; // put your address here
     const balance = await provider.getBalance(address);
     console.log(`Balance of ${address}: ${ethers.formatEther(balance)} ETH`);
 }
--- homework/lesson-1/README.md
+++ homework/lesson-1/README.md
@@ -1,2 +1,9 @@
 # Homework: Lesson 1
 
+1. 安装三种不同的钱包，创建测试账户。
+2. 本地编译Polkadot SDK，启动节点和RPC服务
+3. https://faucet.polkadot.io/?parachain=1111 得到测试token
+4. 分别在钱包中和运行程序 https://github.com/papermoonio/2026-h1-building-production-grade-dApps/blob/main/course/lesson-1/src/index.ts 确认token领取成功
+
+结果截图通过PR提交到 
+https://github.com/papermoonio/2026-h1-building-production-grade-dApps/tree/main/homework/lesson-1
